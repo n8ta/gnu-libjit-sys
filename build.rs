5e4928cdc6ef6a0186e152a486163ae79e8aacf3
Nathaniel Tracy-Amoroso <natetracyamoroso@gmail.com>
2022-06-11 12:18:15 -0700

    switch from spawn

--- build.rs
+++ build.rs
@@ -8,13 +8,13 @@ fn main() {
     env::set_current_dir(path.to_str().expect("expect libjit to exist in cargo outdir")).expect("expected libjit to exist in out dir");
 
     Command::new("./bootstrap")
-        .spawn().expect("Failed to run ./bootstrap installing libjit");
+        .output().expect("Failed to run ./bootstrap installing libjit");
     Command::new("./configure")
-        .spawn().expect("Failed to run ./configure while installing libjit");
+        .output().expect("Failed to run ./configure while installing libjit");
     Command::new("make")
-        .spawn().expect("Failed to run make while installing libjit");
+        .output().expect("Failed to run make while installing libjit");
     Command::new("make").args(&["install"])
-        .spawn().expect("Failed to run make install while installing libjit");
+        .output().expect("Failed to run make install while installing libjit");
 
     let library_path = path.join("jit").join(".libs");
 
