2d75df28629b6a06e50dd0ff8036a846899a98c3
Nathaniel Tracy-Amoroso <natetracyamoroso@gmail.com>
2022-06-11 12:12:24 -0700

    readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [libjit homepage](https://www.gnu.org/software/libjit/)
 
 This project adds raw rust bindings around the libjit package. It is useful as a low startup time jit compiler making it ideal
-for command line tools, interpreter languages, etc.
+for command line tools, interpreted languages, etc. Time to compile a single function that returns a number is about ~5ms on my old laptop.
 
 # Usage
 ```
--- build.rs
+++ build.rs
@@ -3,17 +3,9 @@ use std::env;
 use std::path::{Path, PathBuf};
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
     let path = Path::new("./libjit");
 
-
-    Command::new("cp").args(&["-r", path.to_str().expect("converting ./libjit to path"), &out_dir])
-        .spawn().expect("copying lib jit to cargo outdir");
-
-    let libjit_out_path = PathBuf::from(out_dir).join("libjit");
-
-    env::set_current_dir(libjit_out_path.to_str().expect("expect libjit to exist in cargo outdir")).expect("expected libjit to exist in out dir");
+    env::set_current_dir(path.to_str().expect("expect libjit to exist in cargo outdir")).expect("expected libjit to exist in out dir");
 
     Command::new("./bootstrap")
         .spawn().expect("Failed to run ./bootstrap installing libjit");
@@ -24,7 +16,7 @@ fn main() {
     Command::new("make").args(&["install"])
         .spawn().expect("Failed to run make install while installing libjit");
 
-    let library_path = libjit_out_path.join("jit").join(".libs");
+    let library_path = path.join("jit").join(".libs");
 
     println!("cargo:rustc-link-search={}", library_path.to_str().expect("libjit './jit/.libs' directory was not created during installation for some reason"));
     println!("cargo:rustc-link-lib=jit.0");
